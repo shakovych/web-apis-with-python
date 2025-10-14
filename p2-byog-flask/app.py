7c35ef2129be9a141cf429793c4d33a863cb1627
shakovych <rshakovych@rrc.ca>
2025-10-14 18:59:50 -0500

    Saved evyruthing for the first p1-hello-api-flask app.py

--- p2-byog-flask/app.py
+++ p2-byog-flask/app.py
@@ -7,16 +7,26 @@ def index():
     """
     TODO: Render the home page provided under templates/index.html in the repository
     """
-    return "TODO"
+    return render_template("index.html")
 
 @app.get("/search")
 def search():
-	"""
-	TODO:
-	1. Capture the word that is being searched
-	2. Seach for the word on Google and display results
-	"""
-    return "TODO"
+    """
+    TODO:
+    1. Capture the word that is being searched
+    2. Search for the word on Google and display results
+    """
+    """ return "TODO" """
+    args = request.args.get("q")
+    return redirect(f"https://google.com/search?q={args}")
+
+# I am feeling lucky search form code
+@app.get("/lucky")
+def lucky():
+    query = request.args.get("q")
+    if not query:
+        return "Please enter a search term.", 400
+    return redirect(f"https://www.google.com/search?q={query}&btnI=1")
 
 if __name__ == "__main__":
     app.run()
